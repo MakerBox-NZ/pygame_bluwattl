bec8eb625d837009257a99bfbac6a992bb59f0c8
bluwattl <lukas@berhampore.school.nz>
2017-06-07 17:26:37 +1200

    Renamed Background
    :green_apple: :+1: Background works, renamed background and hero.
    :red_circle: :-1: Sprite does not show, background quarter size.

--- bluwattlgame.py
+++ bluwattlgame.py
@@ -1,29 +1,35 @@
+'''LIBRARIES'''
 import pygame
 import sys
 import os
-
 '''OBJECTS'''
-
-
-
+class Player(pygame.sprite.Sprite):
+    #spawn and dab FAKE
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = [ ]
+        img = pygame.image.load(os.path.join('images','hero.png')).convert()
+        self.images.append(img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
 '''SETUP'''
 screenX = 960
 screenY = 720
-green = [0, 255, 0]
 fps = 40
 afps = 4
 clock = pygame.time.Clock()
-
 pygame.init()
-
 main = True
-
 screen = pygame.display.set_mode([screenX, screenY])
-
+backdrop = pygame.image.load(os.path.join('images','background.png')).convert()
+backdropRect = screen.get_rect()
+player = Player() #Spawn REAL
+player.rect.x = 0
+player.rect.x = 0
+movingsprites = pygame.sprite.Group()
+movingsprites.add(player)
 
 '''LOOP'''
-#screen.fill()
-
 while main == True:
     for event in pygame.event.get():
         if event.type == pygame.KEYUP:
@@ -31,7 +37,6 @@ while main == True:
                 pygame.quit()
                 sys.exit()
                 main = False
-
-    screen.fill(green)
-    pygame.display.flip
+    screen.blit(backdrop, backdropRect)
+    pygame.display.flip()
     clock.tick(fps)
